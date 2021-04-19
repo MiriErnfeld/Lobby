fbba597ba1f57727ea806e80ffe7574f984a3277
MiriErnfeld <miri@leader.codes>
2021-04-19 21:10:16 +0300

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import './App.css';
 import store from './Redux/staticStore';
 import InformationTemp from './components/InformationTemp';
+// import FavIcon from './components/Icon';
 
 
 
@@ -12,8 +13,9 @@ function App() {
   return (
     <>
       <Provider store={store}>
+        {/* <FavIcon></FavIcon> */}
         <InformationTemp />
-    
+
       </Provider>
     </>
   );
---
+++ src/components/Icon.js
@@ -0,0 +1,34 @@
+
+// // import React from 'react';
+// // import { Helmet } from "react-helmet";
+// // import Favicon from 'react-favicon';
+
+// // const FavIcon = (props) => {
+// //     const {card} = props;
+
+// //     return (
+// //         <div>
+// //             <Helmet>
+// //                 <meta charSet="utf-8" />
+// //                 <title>{card.favIcon.title}</title>
+// //             </Helmet>
+// //             <Favicon url={card.favIcon.src||require('../img/iconFour.png')} />
+// //         </div>
+// //     );
+
+// // }
+
+// // export default FavIcon;
+
+// import React from 'react'
+
+// import Favicon from 'react-favicon'
+
+// export default function Icon() {
+//     debugger
+//     return (
+//         <div>
+//             {/* <Favicon url='../img/iconTwo.png' /> */}
+//             <h1>Hello, Favicon!</h1>
+//         </div>)
+// }
