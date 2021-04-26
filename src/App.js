a4ecf0670032427f2e26d65dcaad96e113c10cdf
MiriErnfeld <miri@leader.codes>
2021-04-26 11:39:46 +0300

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -9,19 +9,11 @@ import './App.css';
 import InformationTemp from './components/InformationTemp';
 import ProtectedRoute from './components/ProtectedRoutes'
 
-
-
-
-
-
 function App() {
-
   return (
-
     <Provider store={store}>
       <ProtectedRoute path="/:userName" component={InformationTemp} />
     </Provider>
   );
 }
-
 export default App;
