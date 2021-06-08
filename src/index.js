956c4f3953d46e91ef54d02f54917f32c8249e0f
MiriErnfeld <miri@leader.codes>
2021-06-08 12:56:27 +0300

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Favicon from 'react-favicon';
 
 ReactDOM.render(
   <>
-    <Favicon url='https://oflisback.github.io/react-favicon/img/github.ico' />
+    <Favicon url='https://files.leader.codes/favicon.ico' />
 
     <React.StrictMode>
       <App />
