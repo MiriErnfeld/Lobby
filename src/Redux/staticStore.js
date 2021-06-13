9053859f214ab8a5c7e3d665e29c0be053561861
MiriErnfeld <miri@leader.codes>
2021-06-13 14:49:41 +0300

    --- Auto-Git Commit ---

--- src/Redux/staticStore.js
+++ src/Redux/staticStore.js
@@ -4,7 +4,7 @@ import staticDetailsReducer from './reducers/staticDetailsReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 
-const reducer = combineReducers({ staticDetailsReducer });
+const reducer = combineReducers({ staticDetailsReducer  });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(extractJwt, getStaticData)));
 window.store = store;
